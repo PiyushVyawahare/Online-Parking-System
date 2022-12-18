4c3dd4c476f3fc7dc280f3763bb0e29a8a3d5796
piyus <piyushvyawahare2001@gmail.com>
2022-12-18 16:07:08 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home';
 import './App.css';
+import Register from './pages/register';
+import Login from './pages/login';
+import VerifyUser from './pages/verifyUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/register" element={<Register />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/verifyUser" element={<VerifyUser />}></Route>
+
+      </Routes>
+    </>
   );
 }
 
